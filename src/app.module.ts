0dd73187294f1c88f5ec34e7be4699a110611e8f
Maxime Lenne <maxime.lenne@gmail.com>
2019-01-25 17:52:05 +0100

    initialize typeorm and fastify

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { config } from "dotenv";
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(),
+    config
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly connection: Connection) {}
+}
