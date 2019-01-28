28392b8bfe8686b9e7639b4c2257f3637cf4d39b
Maxime Lenne <maxime.lenne@gmail.com>
2019-01-28 16:15:04 +0100

    add migration on typeOrm + active record entity exemple + npm script for typeorm client

--- ormconfig.js
+++ ormconfig.js
@@ -2,7 +2,7 @@ module.exports = {
   "type": "postgres",
   "url": process.env.DATABASE_URL,
   "entities": ["src/**/**.entity{.ts,.js}"],
-  "migrations": ["src/migrations/*{.ts,.js}"],
+  "migrations": ["src/migrations/*{.js}"],
   "cli": {
       "migrationsDir": "src/migrations"
   }
