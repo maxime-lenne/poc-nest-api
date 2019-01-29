435fa7a2d146c0c138e6941eec34283f06eabb21
Maxime Lenne <maxime.lenne@gmail.com>
2019-01-29 09:52:51 +0100

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 require('newrelic');
 import { NestFactory, FastifyAdapter } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  //App http server
   const app = await NestFactory.create(AppModule, new FastifyAdapter());
   await app.enableCors();
   await app.listen(process.env.PORT, '0.0.0.0');
+
+  //Swagger
+  const options = new DocumentBuilder()
+    .setTitle('Poc nest.js API')
+    .setDescription('The Poc nest.js API description')
+    .setVersion('1.0')
+    .addTag('categories')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
 }
 bootstrap();
